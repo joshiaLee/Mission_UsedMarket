80dad41f0ba2023b8e84b497460765209238bf36
joshiaLee <hhhjs0133@naver.com>
2024-02-28 22:40:46 +0900

    token validate

---
+++ market/src/main/java/com/example/market/TokenController.java
@@ -0,0 +1,57 @@
+package com.example.market;
+
+import com.example.market.jwt.JwtRequestDto;
+import com.example.market.jwt.JwtResponseDto;
+import com.example.market.jwt.JwtTokenUtils;
+import io.jsonwebtoken.Claims;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+@Slf4j
+@RestController
+@RequestMapping("/token")
+@RequiredArgsConstructor
+public class TokenController {
+    private final JwtTokenUtils jwtTokenUtils;
+    private final UserDetailsManager manager;
+    private final PasswordEncoder passwordEncoder;
+
+    @PostMapping("/issue")
+    public JwtResponseDto token(
+            @RequestBody
+            JwtRequestDto dto
+    ){
+        if (!manager.userExists(dto.getUsername()))
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED);
+
+        UserDetails userDetails = manager.loadUserByUsername(dto.getUsername());
+
+        if(!passwordEncoder
+                .matches(dto.getPassword(), userDetails.getPassword()))
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED);
+
+        String jwt = jwtTokenUtils.generateToken(userDetails);
+        JwtResponseDto response = new JwtResponseDto();
+        response.setToken(jwt);
+
+        return response;
+
+    }
+
+    @GetMapping("/validate")
+    public Claims validateToken(
+            @RequestParam("token")
+            String token
+    ){
+        if(!jwtTokenUtils.validate(token))
+            throw new ResponseStatusException(HttpStatus.FORBIDDEN);
+        return jwtTokenUtils.parseClaims(token);
+    }
+
+}
